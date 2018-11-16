9f095a68b012d53add201361f86965d37acec2d5
gayapedro <pedrogaya@gmail.com>
2018-11-16 01:19:48 -0300

    aplicarMaxHeap function created

--- heapsort.c
+++ heapsort.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+void aplicarMaxHeap(int sequencia[], int n, int i){
+    int maiorElemento, filhoEsquerda, filhoDireita, aux;
+    maiorElemento = i;
+    filhoEsquerda = 2*i+1;
+    filhoDireita = 2*i+2;
+    aux = 0;
+    if(filhoEsquerda<n && sequencia[filhoEsquerda]>sequencia[maiorElemento]){
+        maiorElemento = filhoEsquerda;
+    }
+    if(filhoDireita<n && sequencia[filhoDireita]>sequencia[maiorElemento]){
+        maiorElemento = filhoDireita;
+    }
+    if(maiorElemento != i){
+        aux = sequencia[maiorElemento];
+        sequencia[maiorElemento] = sequencia[i];
+        sequencia[i] = aux;
+        aplicarMaxHeap(sequencia,n,maiorElemento);
+    }
+}
+
+int main(){
+    return 0;
+}
