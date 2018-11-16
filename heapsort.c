f099eb5332d87d27b56bed88962e3e5224976a1c
gayapedro <pedrogaya@gmail.com>
2018-11-16 01:33:57 -0300

    heapSort function created

--- heapsort.c
+++ heapsort.c
@@ -20,6 +20,31 @@ void aplicarMaxHeap(int sequencia[], int n, int i){
     }
 }
 
+void heapSort(int sequencia[], int n){
+    int aux;
+    for(int i=n/2-1;i>=0;i--){
+        aplicarMaxHeap(sequencia,n,i);
+    }
+    for(int i=n-1;i>=0;i--){
+        aux = sequencia[i];
+        sequencia[i] = sequencia[0];
+        sequencia[0] = aux;
+        aplicarMaxHeap(sequencia,i,0);
+    }
+}
+
 int main(){
+    int sequencia[6] = {1,12,9,5,6,10},tam;
+    tam = sizeof(sequencia)/sizeof(sequencia[0]);
+    printf("Sequencia desordenada :");
+    for(int i=0;i<tam;i++){
+        printf(" %d",sequencia[i]);
+    }
+    heapSort(sequencia,tam);
+    printf("\nSequencia ordenada    :");
+    for(int i=0;i<tam;i++){
+        printf(" %d",sequencia[i]);
+    }
+    printf("\n");
     return 0;
 }
